0ede7fc20cfa5ff51e036efd37409efdfd405b55
Naitik Pandey <naitikp603@gmail.com>
2026-06-08 22:38:18 +0530

    Add files via upload

---
+++ Day - 8/Q29.java
@@ -0,0 +1,19 @@
+
+// Write a program to Print half pyramid pattern. 
+
+package Summer_Assignment_25113EW009;
+
+public class Q29 {
+
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
